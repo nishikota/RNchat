ee8d2c32a66be127af01bec4a563d2736e779dd1
nishikota <nishi@livicon.jp>
2022-01-07 12:50:10 +0900

    firestore　追加

---
+++ chatapp/src/screens/Top.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Top = () => {
+  return (
+    <View>
+      <Text>Top</Text>
+    </View>
+  );
+};
+
+export default Top;
