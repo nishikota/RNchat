bb1ec8d1b3a6a9b8ce0800726e48cd904071b9fb
nishikota <nishi@livicon.jp>
2022-01-11 09:33:44 +0900

    トップ画面一旦完成

--- chatapp/src/screens/Chat.js
+++ chatapp/src/screens/Chat.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import {getDBdata, registrationDB} from '../api/Database';
 
-const Chat = ({route, navigation}) => {
+const Chat = ({route}) => {
   const [inputContent, setInputContent] = useState('');
   const name = route.params;
   const buttonMove = () => {
@@ -11,17 +11,35 @@ const Chat = ({route, navigation}) => {
     setInputContent('');
   };
   return (
-    <View>
+    <View style={styles.wrapper}>
       <Text>Chat</Text>
       <TextInput onChangeText={setInputContent} value={inputContent} />
       <TouchableOpacity onPress={() => buttonMove()}>
         <Text>Send</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>Exit bye</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 export default Chat;
+
+const styles = {
+  wrapper: {
+    backgroundColor: '#81CFFF',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  title: {
+    backgroundColor: '#0052B2',
+    color: 'white',
+    fontSize: 30,
+    margin: 'auto',
+  },
+  button: {
+    backgroundColor: '#0052B2',
+  },
+  buttonMsg: {
+    fontSize: 20,
+    color: 'white',
+  },
+};
--- chatapp/src/screens/Top.js
+++ chatapp/src/screens/Top.js
@@ -1,23 +1,128 @@
 import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, TextInput} from 'react-native';
 
+const welcomeMsg = name => {
+  if (name) {
+    console.log('ok');
+    return (
+      <View style={styles.welcomeMsgWrapper}>
+        <Text style={styles.welcomeMsgName}>{`OK ${name} !`}</Text>
+        <Text style={styles.welcomeMsgFirst}>Let's Talk !</Text>
+        <Text style={styles.welcomeMsgSecond}>Have fun !</Text>
+      </View>
+    );
+  } else {
+    console.log('no');
+    return (
+      <View>
+        <Text />
+        <Text />
+      </View>
+    );
+  }
+};
+
 const Top = ({navigation}) => {
   const [name, setName] = useState('');
   return (
-    <View>
-      <Text>Who are you ?</Text>
-      <View>
-        <Text>I'm</Text>
-        <TextInput onChangeText={setName} value={name} />
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>Who are you ?</Text>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.inputText}>I'm...</Text>
+        <TextInput
+          onChangeText={setName}
+          value={name}
+          style={styles.inputArea}
+        />
       </View>
-      <Text>{`OK ${name} !`}</Text>
-      <Text>Let's Talk ! Have fun !</Text>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Chat', {name: name})}>
-        <Text>Enter</Text>
-      </TouchableOpacity>
+      {console.log(name)}
+      {welcomeMsg(name)}
+      {name ? (
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Chat', {name: name})}
+          style={styles.button}>
+          <Text style={styles.buttonMsg}>Enter</Text>
+        </TouchableOpacity>
+      ) : (
+        <Text style={styles.buttonMsg}>Tell me your name !</Text>
+      )}
     </View>
   );
 };
 
 export default Top;
+
+const styles = {
+  wrapper: {
+    backgroundColor: '#81CFFF',
+    marginLeft: 10,
+    marginRight: 10,
+    marginVertical: 10,
+    height: 770,
+  },
+  title: {
+    backgroundColor: '#0052B2',
+    color: 'white',
+    fontSize: 30,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    marginVertical: 20,
+    width: '80%',
+    textAlign: 'center',
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  button: {
+    backgroundColor: '#0052B2',
+    width: '30%',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    marginVertical: 15,
+    borderRadius: 20,
+    padding: 3,
+  },
+  buttonMsg: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 20,
+  },
+  inputText: {
+    fontSize: 32,
+    color: 'white',
+  },
+  inputArea: {
+    backgroundColor: 'white',
+    fontSize: 25,
+    borderWidth: 1,
+    width: '50%',
+    padding: 2,
+  },
+  welcomeMsgWrapper: {
+    marginVertical: 30,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  welcomeMsgName: {
+    color: 'white',
+    fontSize: 40,
+    marginBottom: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  welcomeMsgFirst: {
+    color: 'white',
+    fontSize: 30,
+    marginLeft: 70,
+    width: '80%',
+  },
+  welcomeMsgSecond: {
+    color: 'white',
+    fontSize: 30,
+    marginLeft: 180,
+  },
+};
